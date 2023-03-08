78e59b6634e27402fa30ef71ce4f2327c8f86f65
Francisco-GM <francisco.gonzalez.7my@gmail.com>
2023-03-07 22:11:00 -0600

    implementacion del hola mundo

---
+++ src/holaMundo/HolaMundo.java
@@ -0,0 +1,12 @@
+package holaMundo;
+
+public class HolaMundo { // Creacion de una clase publica en java
+
+	public static void main(String[] args) { // El main es la funcion principal que ejecuta el codigo java
+		// TODO Auto-generated method stub
+		System.out.println("Hola Mundo!!!");
+		System.out.println("Les saluda, Francisco :)");
+
+	}
+
+}
